bff0d94f5b97388e9cfa1e7ace9b8d253ff4bbdd
greyphoenix1 <rahmanmukit905@gmail.com>
2025-05-05 00:59:23 -0400

    config stuff

---
+++ springlearn/src/main/java/com/wolviecodes/Dev.java
@@ -0,0 +1,14 @@
+package com.wolviecodes;
+
+
+public class Dev {
+
+    public void build(){
+        System.out.println("I am building!...");
+    }
+    public static void main(String[] args){
+
+
+        System.out.println("HI");
+    }
+}
